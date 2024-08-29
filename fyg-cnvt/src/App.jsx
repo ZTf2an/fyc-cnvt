acafa7437f64c70ba2d583265295b906413751da
steffan <steffan.pardorios@gmail.com>
2024-08-29 16:45:57 -0500

    INICIALIZANDO PROYECTO VITE + REACT + BOOTSTRAP

---
+++ fyg-cnvt/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>PROXIMAMENTE FINANZAS Y GESTION CONECTIVATE</h1>
+    </>
+  )
+}
+
+export default App
