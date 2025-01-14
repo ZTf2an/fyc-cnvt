f1dc760ede6fe24d014c7a990808216f15c08b59
steffan <steffan.pardorios@gmail.com>
2025-01-13 20:25:48 -0500

    Regresando el sistema a web.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -2,9 +2,9 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbzbKY4c15J30vJOfKgXgG4Mph1BYiyTcE62tydAPzPqj3qXdpjCuDWGUjpkCeO8HD9aDA/exec" // falla porque no devuelve success, y con el parametro cot devuelve error de cors
 export const API_GAS = "https://script.google.com/macros/s/AKfycbywAqOck0l_1kw1Bdi_xKGZaHDavXUfx0_XgHlCublvoIlmuGKqv-hQ3angjomaN8rFnA/exec"
 
-export const API_CNVT = "http://localhost:3000/api/v1/registros"
+// export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "http://home.koeficiente.com:3000/api/v1/registros"
-// export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
+export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
 
 /*
 import numberToWords from 'number-to-words'
