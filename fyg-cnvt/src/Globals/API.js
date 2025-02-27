c6804983258e9e4f5c5f4acc1e7320d2f67c9df9
Steffan <Steffan3d7@gmail.com>
2025-02-27 13:27:19 -0500

    NUEVA IMPLEMENTACION GAS: - Por andar jugando me tiré la implementación pero ya sirve.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxwDjaoyKqk73spk61WGtzxRKtnqD32Q_4wubGlrKxh-AlQEYRxan5B7IgIPKY176Z_9g/exec" // V 1.0.11 Se agrega el rut julian
+export const API_GAS = "https://script.google.com/macros/s/AKfycbyyy_l0Jrq14wAs7ptia5ApCGqVg5uPxYpwd5CRYTFdf4qAd-ufD9-0xXKIj1_icu_Biw/exec" // V 1.0.11 Se agrega el rut julian
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
