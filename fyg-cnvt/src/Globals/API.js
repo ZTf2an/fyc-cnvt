bf36f61c86f5dbc369a05894c35e45441baeade8
steffan <steffan.pardorios@gmail.com>
2025-03-25 08:55:31 -0500

    CORRECCION API: -Se corrige el numero de rut enla cuenta de cobro de Steffan. -La correccion se ha hecho en apps script y se actualiza la url en este documento.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,5 @@
-export const API_GAS = "https://script.google.com/macros/s/AKfycbyyy_l0Jrq14wAs7ptia5ApCGqVg5uPxYpwd5CRYTFdf4qAd-ufD9-0xXKIj1_icu_Biw/exec" // V 1.0.11 Se agrega el rut julian
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbyyy_l0Jrq14wAs7ptia5ApCGqVg5uPxYpwd5CRYTFdf4qAd-ufD9-0xXKIj1_icu_Biw/exec" // V 1.0.11 Se agrega el rut julian
+export const API_GAS = "https://script.google.com/macros/s/AKfycbw3VcZGZ-3NOk-fwZNR4eafB2Tuwo0dO1nTXiWJqHQ_AQbQVRdaORvbG73KOyZsHklkAQ/exec" // V Se corrige el Rut en la cuenta Steffan
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
