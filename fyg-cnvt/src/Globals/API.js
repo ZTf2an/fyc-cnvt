c4a1260e7ef0fca1023c091949c1afa387390dc8
steffan <steffan.pardorios@gmail.com>
2025-07-16 17:17:27 -0500

    CORRECCION DE API: - Se actualizó la plantilla de correo de informes en la API. - Se actualizó la url de la implementación.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxwYTp2-39hNlPZEcL_-OaLNz5klvvGsZB156TMWmfS02mz6NQgpl5mVHxCjH3iGnNozw/exec" // V ACTUALIZANDO PLANTILLAS DE CORREOS.
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzm9qzA0ulQoaoI6aK7ch0qo_ATJSUrhVLC83y4dWTSWMYffie0Fy60qD2hMzEot6fqSQ/exec" // V1.1.2 ACTUALIZACION EN PLANTILLA DE CORREO DE INFORMES
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
