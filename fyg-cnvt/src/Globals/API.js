9a7bc25e71912453674c6fbb0fb9ad73072b66c5
steffan <steffan.pardorios@gmail.com>
2025-07-16 16:19:01 -0500

    CORRIGIENDO API: - Se corrige la el diseño de plantillas en la API. - Se actualiza el enlace de la implementacion.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,6 +1,6 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbw3VcZGZ-3NOk-fwZNR4eafB2Tuwo0dO1nTXiWJqHQ_AQbQVRdaORvbG73KOyZsHklkAQ/exec" // V Se corrige el Rut en la cuenta Steffan
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxTcXyEtNqBBiaTA97ZYhLRXmfV-lSIlhmrDKXsxW8jF1XaBS3RCx9i63Q1D4smOoDfpQ/exec" // V iniciando api caribe
+export const API_GAS = "https://script.google.com/macros/s/AKfycbxwYTp2-39hNlPZEcL_-OaLNz5klvvGsZB156TMWmfS02mz6NQgpl5mVHxCjH3iGnNozw/exec" // V ACTUALIZANDO PLANTILLAS DE CORREOS.
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
+//torresdelmarph@gmail.com
