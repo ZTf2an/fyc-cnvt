fcf81d897c37df11082c80fdb5401f465f3d0686
steffan <steffan.pardorios@gmail.com>
2025-07-16 17:52:07 -0500

    ARREGLANDO NAVBAR: -Se hace la migración del navbar a ReactBoostrap para que el navbar sea responsivo.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -3,4 +3,3 @@ export const API_GAS = "https://script.google.com/macros/s/AKfycbzm9qzA0ulQoaoI6
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
-//torresdelmarph@gmail.com
