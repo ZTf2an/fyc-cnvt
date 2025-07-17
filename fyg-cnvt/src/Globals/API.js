199563ce4bfe1340f549de97dfc616ca13a3aef2
steffan <steffan.pardorios@gmail.com>
2025-07-17 13:19:17 -0500

    CORRECCION DE API: -Se actualiza la url de la API --Se corrige la plantilla de cuenta de cobro.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzm9qzA0ulQoaoI6aK7ch0qo_ATJSUrhVLC83y4dWTSWMYffie0Fy60qD2hMzEot6fqSQ/exec" // V1.1.2 ACTUALIZACION EN PLANTILLA DE CORREO DE INFORMES
+export const API_GAS = "https://script.google.com/macros/s/AKfycbz_Mcnko9OzdZL5-7wDoUqetepSIetKxg3oPXl44uuUSR68QtIZr0MpTBS-vs_4kLfGyA/exec" // V1.1.2 ACTUALIZACION EN PLANTILLA DE CORREO DE INFORMES
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
