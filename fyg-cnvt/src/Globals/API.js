4b5df09bb7acf16a117c23b3dc3c25b5c03b77fc
steffan <steffan.pardorios@gmail.com>
2025-08-05 18:03:16 -0500

    ACTUALIZANDO API: - Se actualizan los documentos en la API.  - Se agrega parrafo en cotización explicando el valor con descuento.  - Se agrega parrafo en cuenta explicando el valor de horas extras.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,5 @@
-export const API_GAS = "https://script.google.com/macros/s/AKfycbz_Mcnko9OzdZL5-7wDoUqetepSIetKxg3oPXl44uuUSR68QtIZr0MpTBS-vs_4kLfGyA/exec" // V1.1.2 ACTUALIZACION EN PLANTILLA DE CORREO DE INFORMES
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbz_Mcnko9OzdZL5-7wDoUqetepSIetKxg3oPXl44uuUSR68QtIZr0MpTBS-vs_4kLfGyA/exec" // V1.1.2 ACTUALIZACION EN PLANTILLA DE CORREO DE INFORMES
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzP2F11llr7BU2QnbWHsfCPKxG_xoEQd3u5xe5oTVbDX3sNkXygv0e7uB1JOJ3-RYzNEw/exec" // V1.2 ACTUALIZACION EN DOCUMENTOS DE COTIZACIÓNES Y CUENTAS
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
