49689fca972e1a419f5e91322d37beb981dad16f
steffan <steffan.pardorios@gmail.com>
2025-08-05 19:12:13 -0500

    ACTUALIZANDO API: - Se agregan parrafos de cotizaciones y cuentas.  - Se agrega el valor con descuento en cotizaciones.  - Se agrega item de horas extra en cuenta de cobro.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,5 +1,5 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbw3VcZGZ-3NOk-fwZNR4eafB2Tuwo0dO1nTXiWJqHQ_AQbQVRdaORvbG73KOyZsHklkAQ/exec" // V Se corrige el Rut en la cuenta Steffan
-export const API_GAS = "https://script.google.com/macros/s/AKfycbyhG-yUDZIJZ4PGDFU77Ug4B7RH8L4ExufdKuCIiGuZG6CfQ8F_gNCmW2OJ1_HaY4gUcA/exec" // V Se corrige ortografia de class documento
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbyhG-yUDZIJZ4PGDFU77Ug4B7RH8L4ExufdKuCIiGuZG6CfQ8F_gNCmW2OJ1_HaY4gUcA/exec" // V Se corrige ortografia de class documento
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwl4EkA-rSxsrk_F8sS0XWNH8uI0sVZT8p5g69kngWHVoJrClzr_KCcglWUzkU9cEf4xg/exec" // V Se Actualizan los documentos de cotizaciónes y cuentas
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
