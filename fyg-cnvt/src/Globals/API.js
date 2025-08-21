98630c0709981b965f27b242ecf7d63d480aae71
steffan <steffan.pardorios@gmail.com>
2025-08-21 17:03:21 -0500

    ACTUALIZANDO API. -Se actualiza la immplementación de GAS.  -Se actualiza rut y certificación bancaria.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,5 +1,5 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbwl4EkA-rSxsrk_F8sS0XWNH8uI0sVZT8p5g69kngWHVoJrClzr_KCcglWUzkU9cEf4xg/exec" // V Se Actualizan los documentos de cotizaciónes y cuentas
-export const API_GAS = "https://script.google.com/macros/s/AKfycbznb3BbRbwMDClUf5PGg2vQx-gX0zCOHNREUIAVt3ug6NwWydhCHSnxhvBLaRiRO0KULw/exec" // V1.3 Se Actualizan implementación Nueva Cotización con IVA
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbznb3BbRbwMDClUf5PGg2vQx-gX0zCOHNREUIAVt3ug6NwWydhCHSnxhvBLaRiRO0KULw/exec" // V1.3 Se Actualizan implementación Nueva Cotización con IVA
+export const API_GAS = "https://script.google.com/macros/s/AKfycbxPixSlH9Eoh9zpM5WsNWGQppPmljU34j-sgvecXrWyE45y7QpqnZx03uHB0z5jqVpC/exec" // V1.3.1 se actualiza rut y certificación
 
 // export const API_CNVT = "http://localhost:3000/api/v1/registros"
 // export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
