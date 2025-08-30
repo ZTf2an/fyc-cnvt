1f1fc24e032a076e5102b83b466d5735cb8296d6
steffan <steffan.pardorios@gmail.com>
2025-08-30 15:59:29 -0500

    actualizando API: -Se actualizó el createFolder para que no duplique carpetas si ya hay creadas.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,6 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbznb3BbRbwMDClUf5PGg2vQx-gX0zCOHNREUIAVt3ug6NwWydhCHSnxhvBLaRiRO0KULw/exec" // V1.3 Se Actualizan implementación Nueva Cotización con IVA
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxPixSlH9Eoh9zpM5WsNWGQppPmljU34j-sgvecXrWyE45y7QpqnZx03uHB0z5jqVpC/exec" // V1.3.1 se actualiza rut y certificación
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbxPixSlH9Eoh9zpM5WsNWGQppPmljU34j-sgvecXrWyE45y7QpqnZx03uHB0z5jqVpC/exec" // V1.3.1 se actualiza rut y certificación
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzVgfGTTVxCTw8SJUZww-QrYQ-Af84LN2CI_emSwx66QQkiHPfxT2YT6mKiqOwKdW43PA/exec" // V1.3.2
 
-// export const API_CNVT = "http://localhost:3000/api/v1/registros"
-// export const API_CNVT = "https://api.conectivate.com:3000/api/v1/registros"
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
