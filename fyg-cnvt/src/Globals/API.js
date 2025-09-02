3ea862a6db8a647fc3e7799a5929cd41469b937d
steffan <steffan.pardorios@gmail.com>
2025-09-02 08:50:01 -0500

    ACTUALIZANDO API: -Se corrige el send mail de calss cotizacion en gas.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbxPixSlH9Eoh9zpM5WsNWGQppPmljU34j-sgvecXrWyE45y7QpqnZx03uHB0z5jqVpC/exec" // V1.3.1 se actualiza rut y certificación
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzVgfGTTVxCTw8SJUZww-QrYQ-Af84LN2CI_emSwx66QQkiHPfxT2YT6mKiqOwKdW43PA/exec" // V1.3.2
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwJNw_6ENzLMN80FnkospgDINWzIOn6JsIupycx2rzrJuiNl_lkUEa6yeAYJtZkDIgRAw/exec" // V1.3.2
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
