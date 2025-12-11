dea26bb1ef282822fa61603a390c51fc28751a8b
steffan <steffan.pardorios@gmail.com>
2025-12-11 06:12:12 -0500

    ACTUALIZANDO API: - Se corrige la ortografía de algunas palabras en los documentos. - Se quita la palabra DVD

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbxPixSlH9Eoh9zpM5WsNWGQppPmljU34j-sgvecXrWyE45y7QpqnZx03uHB0z5jqVpC/exec" // V1.3.1 se actualiza rut y certificación
-export const API_GAS = "https://script.google.com/macros/s/AKfycbwJNw_6ENzLMN80FnkospgDINWzIOn6JsIupycx2rzrJuiNl_lkUEa6yeAYJtZkDIgRAw/exec" // V1.3.2
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbwJNw_6ENzLMN80FnkospgDINWzIOn6JsIupycx2rzrJuiNl_lkUEa6yeAYJtZkDIgRAw/exec" // V1.3.2
+export const API_GAS = "https://script.google.com/macros/s/AKfycbxGzvCz03LT8Y6iuhpxWKsQgVtmwKHemYy7Byyr7KGlUDvAl7qYXY1vlq7ikvlOv67l5Q/exec" // V1.3.4 ACTUALIZANDO API, ERRORES DE REDACCION EN DOCUMENTOS
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
