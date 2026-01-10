de2b3bf5b2739e59567755739d96ef1daca28370
steffan <steffan.pardorios@gmail.com>
2026-01-10 16:11:39 -0500

    ACTUALIZANDO API PARA 2026V1.3.4 ACTUALIZANDO AÑO EN COTIZACIÓN

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbwJNw_6ENzLMN80FnkospgDINWzIOn6JsIupycx2rzrJuiNl_lkUEa6yeAYJtZkDIgRAw/exec" // V1.3.2
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxGzvCz03LT8Y6iuhpxWKsQgVtmwKHemYy7Byyr7KGlUDvAl7qYXY1vlq7ikvlOv67l5Q/exec" // V1.3.4 ACTUALIZANDO API, ERRORES DE REDACCION EN DOCUMENTOS
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbxGzvCz03LT8Y6iuhpxWKsQgVtmwKHemYy7Byyr7KGlUDvAl7qYXY1vlq7ikvlOv67l5Q/exec" // V1.3.4 ACTUALIZANDO API, ERRORES DE REDACCION EN DOCUMENTOS
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzSpVimEFwxmrnqKVTv3RmqqYNid4P1ByJtdMVjLkpRBq5vxaKwwCRidgey79axDgye8Q/exec" // V1.3.4 ACTUALIZANDO AÑO EN COTIZACIÓN
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
