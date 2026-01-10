007cdec2efad13f5fe0061089c392351cbcf1794
steffan <steffan.pardorios@gmail.com>
2026-01-10 15:50:15 -0500

    ACTUALIZANDO API: V1.6.1 ACTUALIZANDO AÑO EN COTIZACIÓN - se arregla la src-img del mailbody

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbz10ckj2lSahWE1qrD-9n7Ik-4i5pdLNQLTKZ0zuXDwtjAhy2sPXHG9P1ZZr6dL7GE1Wg/exec" // SOLUCIONANDO BUG CREATE FOLDER
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzYokiNb2zj9tTrSDMJYpsGIHOxPeG9xjy1Yz52Gj7EVW3aXLWaHorMa3ZdEpErUdY94w/exec" // V1.6.1 ACTUALIZANDO AÑO EN COTIZACIÓN - se arregla la src-img del mailbody
+export const API_GAS = "https://script.google.com/macros/s/AKfycbx3fbeayJxEM1FL2WM83q1rzNnw5D7H_VPWZmimi84ibHWhUczfieg-jQYxuIfv1Bp7UQ/exec" // V1.6.1 ACTUALIZANDO AÑO EN COTIZACIÓN - se arregla la src-img del mailbody
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
