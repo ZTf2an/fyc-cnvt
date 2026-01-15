bc46890960bd95952d447321d028a628e5324e2f
steffan <steffan.pardorios@gmail.com>
2026-01-14 19:51:10 -0500

    AGREGANDO API: Se agrega la API y el nuevo logo

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbxGzvCz03LT8Y6iuhpxWKsQgVtmwKHemYy7Byyr7KGlUDvAl7qYXY1vlq7ikvlOv67l5Q/exec" // V1.3.4 ACTUALIZANDO API, ERRORES DE REDACCION EN DOCUMENTOS
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzSpVimEFwxmrnqKVTv3RmqqYNid4P1ByJtdMVjLkpRBq5vxaKwwCRidgey79axDgye8Q/exec" // V1.3.4 ACTUALIZANDO AÑO EN COTIZACIÓN
+export const API_GAS = "https://script.google.com/macros/s/AKfycbz6F6ZUEcHiYkYGtiP5O8978ZzD0n6h72IQc_O4bH6GvCJiXuz5ATByMerwW7UdXyCPxw/exec" // V0.1 inicializando api para pruebas
 
-export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
+export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
