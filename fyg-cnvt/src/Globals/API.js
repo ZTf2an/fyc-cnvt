55bf58a7399388c4ffd969a4dcb7c887e981116e
steffan <steffan.pardorios@gmail.com>
2026-01-19 16:51:38 -0500

    ACTULALIZANDO API: -V1.4.1 AGREGANDO VALOR AGREGADO AL BORRADOR DE ACTA

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,5 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbxGzvCz03LT8Y6iuhpxWKsQgVtmwKHemYy7Byyr7KGlUDvAl7qYXY1vlq7ikvlOv67l5Q/exec" // V1.3.4 ACTUALIZANDO API, ERRORES DE REDACCION EN DOCUMENTOS
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbzSpVimEFwxmrnqKVTv3RmqqYNid4P1ByJtdMVjLkpRBq5vxaKwwCRidgey79axDgye8Q/exec" // V1.3.4 ACTUALIZANDO AÑO EN COTIZACIÓN
-export const API_GAS = "https://script.google.com/macros/s/AKfycbyowQRSy1_7humfMUpeGXzTO5bf1JqpMyIhmF_NbM79PkL_GXCr033HPKHsjRcSbKSbFA/exec" // V1.4 AÑADIENDO LOS CAMPOS DEL CREATE DEL NUEVO COTIZADOR
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbyowQRSy1_7humfMUpeGXzTO5bf1JqpMyIhmF_NbM79PkL_GXCr033HPKHsjRcSbKSbFA/exec" // V1.4 AÑADIENDO LOS CAMPOS DEL CREATE DEL NUEVO COTIZADOR
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwQsetLwSUQb8XPB_KR1DR4Hion5NHicfPyDzJSos5d7oakawMAV4mONLMpdcaCvQnafA/exec" // V1.4.1 AGREGANDO VALOR AGREGADO AL BORRADOR DE ACTA
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
