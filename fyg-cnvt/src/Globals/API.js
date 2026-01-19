8c2b6bb0c26d3a12cd0d8fed7192da8c1da932fd
steffan <steffan.pardorios@gmail.com>
2026-01-18 23:02:42 -0500

    ACTUALIZANDO API QUE REALIZA LAS COTIZACIONES

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,5 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbz6F6ZUEcHiYkYGtiP5O8978ZzD0n6h72IQc_O4bH6GvCJiXuz5ATByMerwW7UdXyCPxw/exec" // V0.1 inicializando api para pruebas
-export const API_GAS = "https://script.google.com/macros/s/AKfycbybv64EWoyi0TWqLOk01N5xpxLnv3Dp3j7THpaBsv3u7xM4G8CiftTESX2NvRtjhT9Q6g/exec" // V0.0.1 inicializando api para pruebas
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbybv64EWoyi0TWqLOk01N5xpxLnv3Dp3j7THpaBsv3u7xM4G8CiftTESX2NvRtjhT9Q6g/exec" // V0.0.1 inicializando api para pruebas
+export const API_GAS = "https://script.google.com/macros/s/AKfycbz_mcSxJOxGXOFCY98olMEuCFdfDDZtf3lFjPPtS3mJYsjywkHNMnI8lKz0TYmre_ssLA/exec" // V0.2 HABILITANDO COTIZACIONES
 
 export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
