e7ad05ffa0a1edc3835c948d914ea645ceb102f1
steffan <steffan.pardorios@gmail.com>
2026-01-21 18:49:58 -0500

    ACTUALIZANDO API: -V1.4.2 AGREGANDO LA NOTA DE COMPROBANTE EN CUENTA Y SE SOLUCIONA EL ERROR DE EL VALOR QUE SALE CORRIDO EN COTIZACION

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbyowQRSy1_7humfMUpeGXzTO5bf1JqpMyIhmF_NbM79PkL_GXCr033HPKHsjRcSbKSbFA/exec" // V1.4 AÑADIENDO LOS CAMPOS DEL CREATE DEL NUEVO COTIZADOR
-export const API_GAS = "https://script.google.com/macros/s/AKfycbwQsetLwSUQb8XPB_KR1DR4Hion5NHicfPyDzJSos5d7oakawMAV4mONLMpdcaCvQnafA/exec" // V1.4.1 AGREGANDO VALOR AGREGADO AL BORRADOR DE ACTA
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbwQsetLwSUQb8XPB_KR1DR4Hion5NHicfPyDzJSos5d7oakawMAV4mONLMpdcaCvQnafA/exec" // V1.4.1 AGREGANDO VALOR AGREGADO AL BORRADOR DE ACTA
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwTKI2I-IXOCYJfefiVH-nS0WuCeZTWsNLQ_Nph7cPXQiRYlfMXSUBLS7QjIPczi1h8XA/exec" // V1.4.2 AGREGANDO LA NOTA DE COMPROBANTE EN CUENTA Y SE SOLUCIONA EL ERROR DE EL VALOR QUE SALE CORRIDO EN COTIZACION
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
