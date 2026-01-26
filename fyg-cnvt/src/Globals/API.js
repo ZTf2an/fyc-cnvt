e16fbe25b64e9c114a73a103e160f955143b8f5c
steffan <steffan.pardorios@gmail.com>
2026-01-26 11:53:46 -0500

    ACUALIZANDO API: -V1.4.3 NUEVAS PLANTILLAS DE CORREO

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,5 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbwTKI2I-IXOCYJfefiVH-nS0WuCeZTWsNLQ_Nph7cPXQiRYlfMXSUBLS7QjIPczi1h8XA/exec" // V1.4.2 AGREGANDO LA NOTA DE COMPROBANTE EN CUENTA Y SE SOLUCIONA EL ERROR DE EL VALOR QUE SALE CORRIDO EN COTIZACION
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzRxgpapp1wtENDAtUlid_S_BzR4qC23MMr-oyg0mYMqRbEcegQ9Rk9wKR4IpoN7SxOLg/exec" // V1.4.2 Se corrigen items referentes a video Beam en Documento.
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbzRxgpapp1wtENDAtUlid_S_BzR4qC23MMr-oyg0mYMqRbEcegQ9Rk9wKR4IpoN7SxOLg/exec" // V1.4.2 Se corrigen items referentes a video Beam en Documento.
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwUogZ98SqHQrfpT281QiFYQDkkr7_W69FU35_07725cy4NplMNhA_oVECyyg0saQqjcg/exec" // V1.4.3 NUEVAS PLANTILLAS DE CORREO.
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
