5ff36ec6f122be5513adf3ce8b602a11965d7a10
steffan <steffan.pardorios@gmail.com>
2026-01-26 08:54:12 -0500

    ACTUALIZANDO API: -V1.4.2 Se corrigen items referentes a video Beam en Documento.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbwQsetLwSUQb8XPB_KR1DR4Hion5NHicfPyDzJSos5d7oakawMAV4mONLMpdcaCvQnafA/exec" // V1.4.1 AGREGANDO VALOR AGREGADO AL BORRADOR DE ACTA
-export const API_GAS = "https://script.google.com/macros/s/AKfycbwTKI2I-IXOCYJfefiVH-nS0WuCeZTWsNLQ_Nph7cPXQiRYlfMXSUBLS7QjIPczi1h8XA/exec" // V1.4.2 AGREGANDO LA NOTA DE COMPROBANTE EN CUENTA Y SE SOLUCIONA EL ERROR DE EL VALOR QUE SALE CORRIDO EN COTIZACION
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbwTKI2I-IXOCYJfefiVH-nS0WuCeZTWsNLQ_Nph7cPXQiRYlfMXSUBLS7QjIPczi1h8XA/exec" // V1.4.2 AGREGANDO LA NOTA DE COMPROBANTE EN CUENTA Y SE SOLUCIONA EL ERROR DE EL VALOR QUE SALE CORRIDO EN COTIZACION
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzRxgpapp1wtENDAtUlid_S_BzR4qC23MMr-oyg0mYMqRbEcegQ9Rk9wKR4IpoN7SxOLg/exec" // V1.4.2 Se corrigen items referentes a video Beam en Documento.
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
