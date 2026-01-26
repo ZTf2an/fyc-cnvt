2ee90c97aa864647ac05a5e6bc2fe3b581657cfd
steffan <steffan.pardorios@gmail.com>
2026-01-26 12:18:18 -0500

    ACTUALIZANDO API: -0.2.7 Se actualiza la plantilla de cuenta de cobro quitando certificación

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,6 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbx_so0Gl60MoE_DPPxnRQjuKGpXRg9-Ic980_U8wx-wvFkThcc4VUZnyCKKfsvQMIrHkg/exec" // V0.2.3
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbyLwJyVcwnpIFF3Bo5_4YVlNde8Polp6XPnucULcVdyxVmkysjtS1P-6avkMoLrGrlKCw/exec" // 0.2.4 Se agrega el valor de controles en el CREATE y se actualiza el documento de cotizaciones.
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbyi8Kk6hMcN52jpomhBIYfr7SwhwgrWtqSIT6xSDtiq2yObT3aEnUq1oeYAySWpyMLlEA/exec" // 0.2.5 Se agregan archivos a la cuenta de cobro
-export const API_GAS = "https://script.google.com/macros/s/AKfycbwS6cro-oEmz6rwOVkvAblZWuP6xQV3mYrjuhrPxx-qmsd8EIyRpO_nzlfFb3XsXjWbLQ/exec" // 0.2.6 Se agrega el indice de Referido
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbwS6cro-oEmz6rwOVkvAblZWuP6xQV3mYrjuhrPxx-qmsd8EIyRpO_nzlfFb3XsXjWbLQ/exec" // 0.2.6 Se agrega el indice de Referido
+export const API_GAS = "https://script.google.com/macros/s/AKfycbx0XdFqYw8GUTMa9iaRaygM0rjx0BMznQSNHwZaey-mrUYLPYF8ftweb3WYrmTd1G3omw/exec" // 0.2.7 Se actualiza la plantilla de cuenta de cobro quitando certificación
 
 export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
