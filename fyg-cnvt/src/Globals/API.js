bf2eeab290e610380036558d5303ec0bf9c7acd5
steffan <steffan.pardorios@gmail.com>
2026-01-26 09:07:22 -0500

    ACTUALIZANDO API: -V1.7.2 Agregando QR code en modalidades.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbx3fbeayJxEM1FL2WM83q1rzNnw5D7H_VPWZmimi84ibHWhUczfieg-jQYxuIfv1Bp7UQ/exec" // V1.6.1 ACTUALIZANDO AÑO EN COTIZACIÓN - se arregla la src-img del mailbody
-export const API_GAS = "https://script.google.com/macros/s/AKfycbw6O_xhXdlRDb8mBpjry8t3ltIZFrjlZ8Zmt1JikGfowVOjV9DsLKkm-k4rbxRTsrCACg/exec" // V1.7.1 IMPLEMENTANDO LOS NUEVOS CAMBIOS SEGÚN NUEVO COTIZADOR PIXEL-COT --- AGREGANDO HORA EN EL CREATE
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbw6O_xhXdlRDb8mBpjry8t3ltIZFrjlZ8Zmt1JikGfowVOjV9DsLKkm-k4rbxRTsrCACg/exec" // V1.7.1 IMPLEMENTANDO LOS NUEVOS CAMBIOS SEGÚN NUEVO COTIZADOR PIXEL-COT --- AGREGANDO HORA EN EL CREATE
+export const API_GAS = "https://script.google.com/macros/s/AKfycbyMObaBV5gx9KzjPTDT55D09k6GUxYmL0tfqfwMyl4qM66Jwd3MHldgoCyZwkPouKuczQ/exec" // V1.7.2 Agregando QR code en modalidades.
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
