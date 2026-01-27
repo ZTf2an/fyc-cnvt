4cf95e307b0794e45b06e5216e2f69cf9c5343f0
steffan <steffan.pardorios@gmail.com>
2026-01-26 21:55:52 -0500

    Se agregan certificados de votaciones: - Se incluye la logica en GestionEditModal, para que se puedan generar los certificados de votaciones desde el editor. - Se actualiza la API: 0.2.8 Agregando Certificacion de votaciones.

--- fyg-cnvt/src/Components/GestionEditModal/index.jsx
+++ fyg-cnvt/src/Components/GestionEditModal/index.jsx
@@ -1,8 +1,9 @@
 import { useContext, useState } from 'react';
-import { Modal , Form , Button, Row, Col } from 'react-bootstrap';
+import { Modal , Form , Button, Row, Col , Container ,Stack} from 'react-bootstrap';
 import { RegistroContext } from '../../Context';
 import { parsedParams } from '../../useServerHooks/useCreate';
 import formatedDate from '../../Utils/formatDate';
+import { API_CNVT , API_GAS } from '../../Globals/API';
 // import Modal from 'react-bootstrap/Modal';
 
 function GestionEditModal () {
@@ -15,6 +16,42 @@ function GestionEditModal () {
         
         let params = parsedParams(e.target);
         editRow(registroToEdit.id , params , 'lote');
+    }; 
+
+    const generarCertificado = (item) => {
+        if (confirm("Va a generar el certificado de votaciones de "+item.cliente+".")) {
+            fetch(API_CNVT+'/globals' , {
+                method : 'POST',
+                headers : {
+                    'Content-Type': 'application/json'
+                },
+                body : JSON.stringify({url : `${API_GAS}?type=createCertVot` , body : {id : item.id , data : item}})
+            })
+            .then(response => response.json())
+            .then(data => {
+                const result = JSON.parse(data);
+                console.log(result);
+                alert(result.msj);
+            })
+            .catch(err => alert(`Error : ${err}`))
+        };
+        setEditGestionModalIsOpen(false)
+    };
+
+    const validateCertVot = (obj) => {
+        let msj = `Falta por agregar:\n`
+        const bolean = (!!obj.modalidad && !!obj.nit && !!obj.fecha && !!obj.driveFolder && !!obj.remitenteCuenta);
+        if (!obj.modalidad) msj += "- Modalidad\n";
+        if (!obj.nit) {msj += "- Nit\n"};
+        if (!obj.fecha) {msj += "- Fecha\n"};
+        if (!obj.driveFolder) {msj += "- Carpeta de Drive\n"};
+        if (!obj.remitenteCuenta) {msj += "- Remitente de cuenta de Cobro"};
+        if (bolean) msj = "Es posible generar el certificado."
+        
+        return { 
+            msj : msj ,
+            isAbled : bolean
+        }
     }; 
 
     return (<Modal show={editGestionModalIsOpen} onHide={handleClose}>
@@ -66,7 +103,29 @@ function GestionEditModal () {
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button type="submit" form="GestionEditForm">Guardar Cambios</Button>
+                <Container fluid>
+                    <Stack direction="horizontal" className="w-full" gap={3}>
+                        { !validateCertVot(registroToEdit).isAbled ?
+                            <Button 
+                            variant="danger" 
+                            type="button"
+                            title={validateCertVot(registroToEdit).msj}
+                            className="pointer"
+                            onClick={e => alert(validateCertVot(registroToEdit).msj)}
+                            > No Habilitado para Certificados.
+                            </Button>:                           
+                            <Button 
+                            variant="outline-success" 
+                            type="button"
+                            title={validateCertVot(registroToEdit).msj}
+                            className="pointer"
+                            onClick={e => generarCertificado(registroToEdit)}
+                            > Generar Certificado de Votaciones.
+                            </Button>
+                        }
+                        <Button type="submit" form="GestionEditForm">Guardar Cambios</Button>
+                    </Stack>
+                </Container>
             </Modal.Footer>
         </Modal>);
 }
