95ee97894b295b4acab1d8b72d2d5accf4a715e3
steffan <steffan.pardorios@gmail.com>
2026-01-30 20:59:14 -0500

    ACTUALIZANDO API: -0.2.11 SE AGREGA LA FECHA EN EL CREATE EN LA COTIZACIÓN.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,6 +1,5 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbw4tqID4cGZIc94m53CrjvDsZ6OzH9BtFmWZ-dAefdBbneN4LtXpUHZcX7GTpe0RqH_/exec" // 0.2.8 Agregando Certificacion de votaciones.
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbysF98nG8Yg3K-ekDoxb-4UDD7LxetSgARsfs0sJijkn8gcQ7nQtaIJyCavvh_5ZwI/exec" // 0.2.9 CORRIGIENDO ERROR QUE SE GENERÓ EN CLASE COTIZACIÓN.
-export const API_GAS = "https://script.google.com/macros/s/AKfycbyuOU7rOPTvRqnmGtYu15uNQom47adhcHKV8QkR-WiTz3drhidViutt92Vc8SVzZ0srfA/exec" // 0.2.10 SE CORRIGE LA LOGICA QUE CREABA EL LA COTIZACIÓN AGREGANDO QR CODE
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbyuOU7rOPTvRqnmGtYu15uNQom47adhcHKV8QkR-WiTz3drhidViutt92Vc8SVzZ0srfA/exec" // 0.2.10 SE CORRIGE LA LOGICA QUE CREABA EL LA COTIZACIÓN AGREGANDO QR CODE
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwnN1xkKwEyq8QbfuxGY9AmKj0Dzf2ziLuazA9uV-87Xdws1z9XVu0KuIW3ExiUN3iRTw/exec" // 0.2.11 SE AGREGA LA FECHA EN EL CREATE EN LA COTIZACIÓN
 
 
 export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
