b531609bbfcedc35f860c905dcd3227fb478ad2b
ZTf2an <steffan.pardorios@gmail.com>
2026-02-05 12:50:50 -0500

    Actualizar generateExcel.js
    Agrgando administrador al excel

--- fyg-cnvt/src/Utils/generateExcel.js
+++ fyg-cnvt/src/Utils/generateExcel.js
@@ -14,6 +14,7 @@ const pixelFormat = ( obj ) => {
         PREDIOS : obj.predios ,
         CONTROLES : obj.numeroControles ,
         EMAIL : obj.email,
+        ADMINISTRADOR: obj.titular,
         TEL : obj.tel ,
         Dirección : obj.direccion ,
         OBSERVACIÓN : obj.notas
