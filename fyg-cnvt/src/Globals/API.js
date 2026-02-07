cbe8f69e5269c8edd773a3391d605b48abaa4b0a
steffan <steffan.pardorios@gmail.com>
2026-02-06 19:07:02 -0500

    ACTUALIZANDO API: - Se arreglan nuevamente las fechas porque parece que no sirvió el ultimo arreglo.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,5 +1,5 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbwnN1xkKwEyq8QbfuxGY9AmKj0Dzf2ziLuazA9uV-87Xdws1z9XVu0KuIW3ExiUN3iRTw/exec" // 0.2.11 SE AGREGA LA FECHA EN EL CREATE EN LA COTIZACIÓN
-export const API_GAS = "https://script.google.com/macros/s/AKfycbz1HWnbKGwlWL5M2b5jVReyJ0IgFStIS7zCPt_qGVB2PI4H8qc0cWB7G7Ecsa1OSeDarA/exec" // 0.2.12 SOLUCIONANDO LOS FORMATOS DE FECHAS 
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbz1HWnbKGwlWL5M2b5jVReyJ0IgFStIS7zCPt_qGVB2PI4H8qc0cWB7G7Ecsa1OSeDarA/exec" // 0.2.12 SOLUCIONANDO LOS FORMATOS DE FECHAS 
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwc3GmS_zFIvjoBifDZs6qPICs3wbncOqZvg3A7PuFM6o5ZNNTBgwuXHNmraWbhhPUwnQ/exec" // 0.2.12.1 SOLUCIONANDO LOS FORMATOS DE FECHAS CON TIMEZONE UTC
 
 
 export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
