6cda68cedaae422df50676350023164b0763acc0
steffan <steffan.pardorios@gmail.com>
2026-02-12 12:14:36 -0500

    ACTUALIZANDO API: -V1.4.5 SE ACTUALIZA EL MAILTEMPLATE DE CUENTA DE COBRO.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbwUogZ98SqHQrfpT281QiFYQDkkr7_W69FU35_07725cy4NplMNhA_oVECyyg0saQqjcg/exec" // V1.4.3 NUEVAS PLANTILLAS DE CORREO.
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzd8hVV-aQIGuuLNG0wstQOIbFRJsL7sz7niIAy6KmEjn4MK_7hfniohuQhF9H8HIhRnw/exec" // V1.4.4 ACTUALIZANDO EL ID DEL FOLDER PARA EL AÑO 2026
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbzd8hVV-aQIGuuLNG0wstQOIbFRJsL7sz7niIAy6KmEjn4MK_7hfniohuQhF9H8HIhRnw/exec" // V1.4.4 ACTUALIZANDO EL ID DEL FOLDER PARA EL AÑO 2026
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzbPxV1vRLewbRDnVfpieYh8h9ngxqrocjTYP9ZzuZGnGx5IIhho0-QILWK9msSKZ4tyw/exec" // V1.4.5 SE ACTUALIZA EL MAILTEMPLATE DE CUENTA DE COBRO.
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
