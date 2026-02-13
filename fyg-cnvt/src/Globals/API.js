6f48840f79803c2e70c81e2df0098dbf67eafb1f
steffan <steffan.pardorios@gmail.com>
2026-02-13 10:44:41 -0500

    ACTUALIZANDO API: -Se corrige una constante en la api. -0.3 AGREGANDO NUEVAS MODALIDADES EN EL CREATE

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,5 +1,5 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbwc3GmS_zFIvjoBifDZs6qPICs3wbncOqZvg3A7PuFM6o5ZNNTBgwuXHNmraWbhhPUwnQ/exec" // 0.2.12.1 SOLUCIONANDO LOS FORMATOS DE FECHAS CON TIMEZONE UTC
-export const API_GAS = "https://script.google.com/macros/s/AKfycbzUzu44jUwojGD_p4_cmHvAiKEdGRdxsAu1gOyZr-TaAPVo-40J5iaF9NA3zbiChVqExQ/exec" // 0.3 AGREGANDO NUEVAS MODALIDADES EN EL CREATE
+export const API_GAS = "https://script.google.com/macros/s/AKfycbySy5-DRtU-Csy6IIKLErTuQH6pUjgWdSztmZh-ZbvvEDgWHvMhZSIRHfN7oXmYVOTT8Q/exec" // 0.3 AGREGANDO NUEVAS MODALIDADES EN EL CREATE
 
 
 export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
