3940d026ecfb0ee669d21c25aa37ba8e58f9f073
steffan <steffan.pardorios@gmail.com>
2026-02-17 14:40:22 -0500

    ACTUALIZANDO API: 0.3.1 GLOBALIZANDO VALOR EXTRA EN CUENTA DE COBRO

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,5 +1,4 @@
-// export const API_GAS = "https://script.google.com/macros/s/AKfycbwc3GmS_zFIvjoBifDZs6qPICs3wbncOqZvg3A7PuFM6o5ZNNTBgwuXHNmraWbhhPUwnQ/exec" // 0.2.12.1 SOLUCIONANDO LOS FORMATOS DE FECHAS CON TIMEZONE UTC
-export const API_GAS = "https://script.google.com/macros/s/AKfycbySy5-DRtU-Csy6IIKLErTuQH6pUjgWdSztmZh-ZbvvEDgWHvMhZSIRHfN7oXmYVOTT8Q/exec" // 0.3 AGREGANDO NUEVAS MODALIDADES EN EL CREATE
-
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbySy5-DRtU-Csy6IIKLErTuQH6pUjgWdSztmZh-ZbvvEDgWHvMhZSIRHfN7oXmYVOTT8Q/exec" // 0.3 AGREGANDO NUEVAS MODALIDADES EN EL CREATE
+export const API_GAS = "https://script.google.com/macros/s/AKfycbzCtlPu331fDXL3WSgqMqVwFFMPTbZ8_MnUmw_n3HyDOYqELOEa8xfofx9IuG5wzWrGIA/exec" // 0.3.1 GLOBALIZANDO VALOR EXTRA EN CUENTA DE COBRO
 
 export const API_CNVT = "https://dict4lkn79.execute-api.us-east-1.amazonaws.com/"
