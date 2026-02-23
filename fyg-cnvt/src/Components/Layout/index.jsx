13dd58f57756b0d0f82f25bd8c80eaf1e2bdfd35
steffan <steffan.pardorios@gmail.com>
2026-02-22 22:01:15 -0500

    AGREGANDO TOASTS: Se agregan los toast en el layout general. -Se agrega toda la logica necesaria para que funcionen los toast. -Se actualizan las funciones de edit Row para que lance el toast.

--- fyg-cnvt/src/Components/Layout/index.jsx
+++ fyg-cnvt/src/Components/Layout/index.jsx
@@ -1,6 +1,21 @@
+import { useContext } from 'react'
+import {ToastAlerter} from '../Toast'
+import { ToastContainer } from 'react-bootstrap'
+import { RegistroContext  } from '../../Context'
 function Layout ({children}) {
+    const {activeToast} = useContext(RegistroContext)
     return (
         <div className="container-fluid my-2">
+            <ToastContainer className="mb-1 me-1" position='bottom-end'>
+                { activeToast.length > 0 && 
+                activeToast.map((obj , idx) => {
+                    return <ToastAlerter 
+                        key={idx} 
+                        msj={obj.msj}
+                        status={obj.status}
+                    />
+                })}
+            </ToastContainer>
             {children}
         </div>
     )
---
+++ fyg-cnvt/src/Components/Toast/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import {Toast,} from 'react-bootstrap';
+
+export function ToastAlerter ({msj , status}) {
+    const [show, setShow] = useState(true);
+    const toggleShow = () => setShow(!show);
+    const type = {
+        "error": {bg :"danger" , delay:10},
+        "success" : {bg: "success",  delay: 2},
+    }
+
+    return (
+        <Toast 
+            bg={type[status.toLowerCase()].bg} 
+            show={show} 
+            autohide={true}
+            onClose={toggleShow}
+            delay={type[status.toLowerCase()].delay * 1000}
+        >
+            <Toast.Header>
+                <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                <strong className="me-auto">{status}</strong>
+                {/* <small>11 mins ago</small> */}
+            </Toast.Header>
+            <Toast.Body style={{color : 'white'}}>{msj}</Toast.Body>
+        </Toast>
+    )
+}
