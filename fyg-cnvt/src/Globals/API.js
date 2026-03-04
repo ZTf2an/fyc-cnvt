d6f2368db8e18e2f56897f2a6cd2951cf7e191fb
Steffan <Steffan3d7@gmail.com>
2026-03-04 10:24:14 -0500

    ACTUALIZANDO API: -1.7.5 ACUALIZANDO ENCABEZADOS DE DOCUMENTOS CON BARRANQUILLA EN LUGAR DE BOG--

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,5 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbx5hltgjzBshlML3iVdKqxyIj-FRpm51FjPAyISix-ecaiW_FCgr2gewkV6BJC2Pm0UtA/exec" // V1.7.2 Solucionando los errores al generar cotización
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxX0dW12Q_CevQPKQufoHXB_tdOsIv-4hZtNHXq-115qGe3tJhN7cf5w2lRgchoZFD3pg/exec" // 1.7.4 AGREGANDO MODALIDAD DE QR EN EL CREATE PARA QUE FUNCIONE EL DOCUMENTO.
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbxX0dW12Q_CevQPKQufoHXB_tdOsIv-4hZtNHXq-115qGe3tJhN7cf5w2lRgchoZFD3pg/exec" // 1.7.4 AGREGANDO MODALIDAD DE QR EN EL CREATE PARA QUE FUNCIONE EL DOCUMENTO.
+export const API_GAS = "https://script.google.com/macros/s/AKfycbznnJss4pjpe9p-FcvqualHkCtw79CesCK8YAt9_jjuUuOzxZRM9Fx0Ginaq0MbnsvX/exec" // 1.7.5 ACUALIZANDO ENCABEZADOS DE DOCUMENTOS CON BARRANQUILLA EN LUGAR DE BOG--
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
