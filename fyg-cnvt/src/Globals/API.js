635892c494170b17794e2058c73e3faa2b849346
steffan <steffan.pardorios@gmail.com>
2026-03-04 17:55:16 -0500

    ARREGLANDO ALGO QUE NO SE QUE FUE

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbzbPxV1vRLewbRDnVfpieYh8h9ngxqrocjTYP9ZzuZGnGx5IIhho0-QILWK9msSKZ4tyw/exec" // V1.4.5 SE ACTUALIZA EL MAILTEMPLATE DE CUENTA DE COBRO.
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxqOoX0SyeuXxqm9vpxYTAJgsB0Di-A5bvMJ2I-WVHVTP9BWWOhd3l6ERncqsEfyKiJHA/exec" // V1.4.5 SE ACTUALIZA EL MAILTEMPLATE DE CUENTA DE COBRO.
+export const API_GAS = "https://script.google.com/macros/s/AKfycbxqOoX0SyeuXxqm9vpxYTAJgsB0Di-A5bvMJ2I-WVHVTP9BWWOhd3l6ERncqsEfyKiJHA/exec" // 1.4.6 ACTUALIZANDO RUT
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
