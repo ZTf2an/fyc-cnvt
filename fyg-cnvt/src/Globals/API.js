ac6478ed162ff6555c8ec8853a45d9f71ffc21c0
steffan <steffan.pardorios@gmail.com>
2026-03-05 08:44:02 -0500

    ACTUALIAZNDO API:

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,4 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbw6O_xhXdlRDb8mBpjry8t3ltIZFrjlZ8Zmt1JikGfowVOjV9DsLKkm-k4rbxRTsrCACg/exec" // V1.7.1 IMPLEMENTANDO LOS NUEVOS CAMBIOS SEGÚN NUEVO COTIZADOR PIXEL-COT --- AGREGANDO HORA EN EL CREATE
-export const API_GAS = "https://script.google.com/macros/s/AKfycbyyz887Gyt41Y9lm_QJeOvbqUiwp8pidsncyafgHg7NcxT9gKlt3ojn5Y2ehDpPjF-I6w/exec" // 1.7.7 CORRIGIENDO EL ARRAY DE CERTIFICACIONES
+export const API_GAS = "https://script.google.com/macros/s/AKfycbyahMaNNOWHapetwcn0bsFkfY2djU37UcUrKG4U4Bg8wqEZ3mupf2LCPLzCCx3FHCiBLg/exec" // 1.7.7 CORRIGIENDO EL ARRAY DE CERTIFICACIONES
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
