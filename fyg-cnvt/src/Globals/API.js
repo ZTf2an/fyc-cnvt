5b93c05a7a1e39a1f48835f4cab96f46619d47b3
steffan <steffan.pardorios@gmail.com>
2026-03-13 08:04:27 -0500

    ACTUALIZANDO API: -1.4.7 ATENCION, SE LE QUITAN LOS PERMISOS AL PDF, PARECE QUE ESTÁN FUNCIONANDO POR ALGUNA CONFIGURACION POR DEFECTO.

--- fyg-cnvt/src/Globals/API.js
+++ fyg-cnvt/src/Globals/API.js
@@ -1,4 +1,5 @@
 // export const API_GAS = "https://script.google.com/macros/s/AKfycbzbPxV1vRLewbRDnVfpieYh8h9ngxqrocjTYP9ZzuZGnGx5IIhho0-QILWK9msSKZ4tyw/exec" // V1.4.5 SE ACTUALIZA EL MAILTEMPLATE DE CUENTA DE COBRO.
-export const API_GAS = "https://script.google.com/macros/s/AKfycbxqOoX0SyeuXxqm9vpxYTAJgsB0Di-A5bvMJ2I-WVHVTP9BWWOhd3l6ERncqsEfyKiJHA/exec" // 1.4.6 ACTUALIZANDO RUT
+// export const API_GAS = "https://script.google.com/macros/s/AKfycbxqOoX0SyeuXxqm9vpxYTAJgsB0Di-A5bvMJ2I-WVHVTP9BWWOhd3l6ERncqsEfyKiJHA/exec" // 1.4.6 ACTUALIZANDO RUT
+export const API_GAS = "https://script.google.com/macros/s/AKfycbwZPBXD5LGqcjifsYzWWdKgBkqAaW9FhzsV-e7Y0Be-sNPRWuO1bTWVtzxZCLy9rj19/exec" // 1.4.7 ATENCION, SE LE QUITAN LOS PERMISOS AL PDF, PARECE QUE ESTÁN FUNCIONANDO POR ALGUNA CONFIGURACION POR DEFECTO.
 
 export const API_CNVT = "https://izp79zzocj.execute-api.us-east-1.amazonaws.com"
